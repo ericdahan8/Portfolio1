e984f9ebf49882f3764e9420d8ecd0883da23317
Eric Dahan <Ericdahan@MacBook-Pro-2.local>
2019-03-16 01:00:46 -0400

    Added some API - weather and nasa

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -22,3 +22,58 @@ $(window).on("load", function() {
     })
     .scroll(); //invoke scroll-handler on page-load
 });
+
+let city = "New York";
+
+let APIkeyWeather = "70c19500496f9aecbd97ff861d3072ee";
+let queryURLWeather =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&units=imperial&appid=" +
+  APIkeyWeather;
+
+// Weather display API
+$.ajax({
+  url: queryURLWeather,
+  method: "GET"
+}).then(function(response) {
+  let temperatureAndCity = $("<p style='text-align:center;'>").text(
+    response.name.toUpperCase() + ": " + response.main.temp + "° F "
+  );
+  temperatureAndCity.attr("class", "displayTemp");
+  let arr = [];
+  for (let i = 0; i < response.weather.length; i++) {
+    arr.push(response.weather[i].icon);
+  }
+  for (let i = 0; i < 1; i++) {
+    let imgWeather = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/w/" + arr[i] + ".png"
+    );
+    imgWeather.css("filter", "grayscale(100%)");
+    $("#weather").append(imgWeather);
+  }
+  $("#weather").append(temperatureAndCity);
+});
+
+//nasa API
+
+var url =
+  "https://api.nasa.gov/planetary/apod?api_key=JLYllqlcc7KdrBRVuasCBeRkrPZXqJ5vsfbhXTsm";
+
+$.ajax({
+  url: url,
+  success: function(result) {
+    if (result.media_type == "video") {
+      $("#apod_img_id").css("display", "none");
+      $("#apod_vid_id").attr("src", result.url);
+    } else {
+      $("#apod_vid_id").css("display", "none");
+      $("#apod_img_id").attr("src", result.url);
+    }
+    $("#reqObject").text(url);
+    $("#returnObject").text(JSON.stringify(result, null, 4));
+
+    $("#apod_title").text(result.title);
+  }
+});
