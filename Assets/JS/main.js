b1bc95f47a8fc90f4bda95a7091be6b974c9fb7b
Eric Dahan <Ericdahan@MacBook-Pro-2.local>
2019-03-18 00:26:12 -0400

    final deploy

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -63,6 +63,7 @@ var url =
 
 $.ajax({
   url: url,
+
   success: function(result) {
     if (result.media_type == "video") {
       $("#apod_img_id").css("display", "none");
@@ -77,3 +78,35 @@ $.ajax({
     $("#apod_title").text(result.title);
   }
 });
+
+// var url =
+//   "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo";
+
+// $.ajax({
+//   url: url,
+//   success: function(result) {
+//     if ("copyright" in result) {
+//       $("#copyright").text("Image Credits: " + result.copyright);
+//     } else {
+//       $("#copyright").text("Image Credits: " + "Public Domain");
+//     }
+
+//     if (result.media_type == "video") {
+//       $("#apod_img_id").css("display", "none");
+//       $("#apod_vid_id").attr("src", result.url);
+//     } else {
+//       $("#apod_vid_id").css("display", "none");
+//       $("#apod_img_id").attr("src", result.url);
+//     }
+//     $("#reqObject").text(url);
+//     $("#returnObject").text(JSON.stringify(result, null, 4));
+//     $("#apod_explaination").text(result.explanation);
+//     $("#apod_title").text(result.title);
+//   }
+// });
+
+//Audio play
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
